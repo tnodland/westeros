0c5289d199f36f22a9a2e9c33437b245f9799e3b
Trevor Nodland <trevor.nodland@gmail.com>
2019-07-19 10:11:56 -0600

    completes service spec

--- app/services/westeros_service.rb
+++ app/services/westeros_service.rb
@@ -1,5 +1,10 @@
 class WesterosService
   def house_members(house)
-    binding.pry
+    conn = Faraday.new("http://westerosapi.herokuapp.com/api/v1/") do |f|
+      f.adapter Faraday.default_adapter
+    end
+    key = ENV['WESTEROS_KEY']
+    response = conn.get("house/#{house}?api_key=#{key}")
+    return JSON.parse(response.body, symbolize_names: true)[:data][0]
   end
 end
