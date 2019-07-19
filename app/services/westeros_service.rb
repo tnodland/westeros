c2b5127daa2f2575ff8938795ea3158551240e99
Trevor Nodland <trevor.nodland@gmail.com>
2019-07-19 10:25:54 -0600

    refactors connection to be its' own method in service, refactors the member api call so be memoized in the case of a search to the same house

--- app/services/westeros_service.rb
+++ app/services/westeros_service.rb
@@ -1,10 +1,17 @@
 class WesterosService
   def house_members(house)
-    conn = Faraday.new("http://westerosapi.herokuapp.com/api/v1/") do |f|
-      f.adapter Faraday.default_adapter
-    end
     key = ENV['WESTEROS_KEY']
-    response = conn.get("house/#{house}?api_key=#{key}")
-    return JSON.parse(response.body, symbolize_names: true)[:data][0]
+    return JSON.parse(get_members(house, key).body, symbolize_names: true)[:data][0]
   end
+
+  private
+    def conn
+      @_conn = Faraday.new("http://westerosapi.herokuapp.com/api/v1/") do |f|
+        f.adapter Faraday.default_adapter
+      end
+    end
+
+    def get_members(house, key)
+      @_response ||= conn.get("house/#{house}?api_key=#{key}")
+    end
 end
