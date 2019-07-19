4663bc8da005d576c5e25bb7e126010a55cfaa13
Trevor Nodland <trevor.nodland@gmail.com>
2019-07-19 10:44:03 -0600

    fully converts to new api, removes house method in favor of changing the parameters set by search links

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -3,23 +3,22 @@ class SearchFacade
     @house_id = house_id
   end
 
-  def house
-    if @house == "stark"
-      1
-    elsif @house == "lannister"
-      2
-    elsif @house == "targaryen"
-      3
-    elsif @house == "tyrell"
-      4
-    elsif @house == "greyjoy"
-      5
-    end
-  end
+  # def house
+  #   if @house == "stark"
+  #     1
+  #   elsif @house == "lannister"
+  #     2
+  #   elsif @house == "targaryen"
+  #     3
+  #   elsif @house == "tyrell"
+  #     4
+  #   elsif @house == "greyjoy"
+  #     5
+  #   end
+  # end
 
   def members
-    data = service.house_members(house)
-    member_data = data[:attributes][:members]
+    member_data = service.house_members(@house_id)
     member_data.map do |member|
       HouseMember.new(member)
     end
